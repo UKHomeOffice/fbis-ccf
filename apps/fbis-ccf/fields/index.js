d58e43b0f3616e4375437d44e584f93cf3cd1bee
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2020-10-26 15:24:56 +0000

    wip: add placeholder question page

--- apps/fbis-ccf/fields/index.js
+++ apps/fbis-ccf/fields/index.js
@@ -1,18 +1,36 @@
 'use strict';
 
 module.exports = {
-  'query': {
+  query: {
     mixin: 'textarea',
     validate: ['required', { type: 'maxlength', arguments: 500 }],
   },
-  'name': {
+  name: {
     validate: ['required'],
   },
-  'email': {
+  email: {
     validate: ['required', 'email'],
   },
-  'phone': {},
+  phone: {},
   'application-number': {
     validate: ['required'],
-  }
+  },
+  question: {
+    mixin: 'radio-group',
+    validate: ['required'],
+    options: {
+      status: {
+        label: 'Viewing or proving your immigration status'
+      },
+      account: {
+        label: 'Updating your immigration account details'
+      },
+      'id-check': {
+        label: 'The \'ID check\' app'
+      }
+    },
+    legend: {
+      className: 'visuallyhidden'
+    }
+  },
 };
--- apps/fbis-ccf/index.js
+++ apps/fbis-ccf/index.js
@@ -15,6 +15,7 @@ module.exports = {
     },
     '/question': {
       behaviours: [addLocationToBacklink],
+      fields: ['question'],
       next: '/query'
     },
     '/query': {
