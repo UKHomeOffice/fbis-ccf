90b3ed07380f4cb5dc7e7e97e41eeccc31b3dbd2
Mark Wimpory <mark.wimpory@digital.homeoffice.gov.uk>
2020-11-06 08:52:01 +0000

    test - deployment checker 8

--- bin/check_deployment.sh
+++ bin/check_deployment.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 echo "Checking deployment of ${DRONE_COMMIT_SHA} from build ${DRONE_BUILD_PARENT} ${PRODUCTION_RELEASE_KEY}"
+if [ "${PRODUCTION_RELEASE_KEY}" == "${SECRET}" ]
+  then
+    echo "We are a go!"
+  else
+    echo "No way...."
+fi
