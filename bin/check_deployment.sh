25c73cf98ab7a70b0eb2949e77145dc11b938909
Mark Wimpory <mark.wimpory@digital.homeoffice.gov.uk>
2020-11-06 09:19:41 +0000

    test - deployment checker 9

--- bin/check_deployment.sh
+++ bin/check_deployment.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo "Checking deployment of ${DRONE_COMMIT_SHA} from build ${DRONE_BUILD_PARENT} ${PRODUCTION_RELEASE_KEY}"
+echo "Checking deployment of ${DRONE_COMMIT_SHA} from build ${DRONE_BUILD_PARENT} ${PRODUCTION_RELEASE_KEY} ${SECRET}"
 if [ "${PRODUCTION_RELEASE_KEY}" == "${SECRET}" ]
   then
     echo "We are a go!"
   else
-    echo "No way...."
+    echo "Invalid deployment key"
+    exit 1
 fi
