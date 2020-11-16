5935e599b2c1ced510b25717d5871adee6432f0c
Max O'Sullivan <Max.OSullivan@digital.homeoffice.gov.uk>
2020-11-16 14:33:32 +0000

    feat: refactor behaviour to include utils functions to ensure feedback email is only sent once

--- apps/feedback/behaviours/feedback.js
+++ apps/feedback/behaviours/feedback.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const notify = require('../../../config').notify;
-const sendEmail = require('../../../lib/utils').sendEmail;
+const utils = require('../../../lib/utils');
 const uuidv4 = require('uuid').v4;
 
-module.exports = superclass => class extends superclass {
+module.exports = superclass => class Feedback extends superclass {
 
   getValues(req, res, next) {
 
@@ -19,22 +19,37 @@ module.exports = superclass => class extends superclass {
   }
 
   saveValues(req, res, next) {
+    let reference = req.sessionModel.get(notify.submitEmailSessionName);
 
-    const reference = uuidv4();
+    if (reference) {
+      return utils.pollEmailStatus(reference, 0, notify.statusRetryInterval)
+        .then(() => Feedback.handleSuccess(req, next, reference, false))
+        .catch(err => Feedback.handleError(req, next, reference, err));
+    }
 
-    return sendEmail(notify.templateFeedback, notify.feedbackEmail, reference, {
+    reference = uuidv4();
+    req.sessionModel.set(notify.submitEmailSessionName, reference);
+    req.session.save();
+
+    return utils.sendEmail(notify.templateFeedback, notify.feedbackEmail, reference, {
       feedbackRating: req.form.values.feedbackRating,
       feedbackText: req.form.values.feedbackText,
       feedbackEmail: req.form.values.feedbackEmail
     })
-      .then(() => {
-        req.log('info', 'Feedback sent to feedback address', `reference=${reference}`);
-        return super.saveValues(req, res, next);
-      })
-      .catch(err => {
-        req.log('error', 'Error sending feedback email to feedback address', `reference=${reference}`, err);
-        return next(err);
-      });
+      .then(() => Feedback.handleSuccess(req, next, reference, true))
+      .catch(err => Feedback.handleError(req, next, reference, err));
+  }
+
+  static handleSuccess(req, next, reference, shouldLog) {
+    if (shouldLog) {
+      req.log('info', 'Feedback sent to feedback address', `reference=${reference}`);
+    }
+    return next();
+  }
+
+  static handleError(req, next, reference, err) {
+    req.log('error', 'Error sending feedback email to feedback address', `reference=${reference}`, err);
+    return next(err);
   }
 
 };
