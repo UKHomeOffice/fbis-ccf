a5175c60416fab9fa6c123770e8255d01794485d
Mark Wimpory <mark.wimpory@digital.homeoffice.gov.uk>
2020-11-30 13:48:51 +0000

    master - small change to create secrets file, used mostly for reference

--- kube/create_secrets.sh
+++ kube/create_secrets.sh
@@ -1,2 +1,3 @@
 kubectl create secret generic fbis-notify-key --from-literal=fbis-notify-key=${NOTIFY_KEY}
 kubectl create secret generic redis --from-literal=session_secret=${SESSION_SECRET}
+kubectl create secret generic feedback-email --from-literal=feedback-email=${FEEDBACK_EMAIL}
