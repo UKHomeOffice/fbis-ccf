58993eac678ecab8e4afaf47a456304ff0f84392
AlexFranklinHODDaT <alex.franklin@digital.homeoffice.gov.uk>
2020-12-15 10:12:13 +0000

    chore: add casework email to create secrets script

--- kube/create_secrets.sh
+++ kube/create_secrets.sh
@@ -1,3 +1,4 @@
 kubectl create secret generic fbis-notify-key --from-literal=fbis-notify-key=${NOTIFY_KEY}
 kubectl create secret generic redis --from-literal=session_secret=${SESSION_SECRET}
 kubectl create secret generic feedback-email --from-literal=feedback-email=${FEEDBACK_EMAIL}
+kubectl create secret generic fbis-casework-email --from-literal=fbis-casework-email=${SRC_CASEWORK_EMAIL}
