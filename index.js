b9d8264110a869c9cc18a0c6dc30618c1703c7d8
AlexFranklinHODDaT <alex.franklin@digital.homeoffice.gov.uk>
2020-12-17 11:38:33 +0000

    refactor: top level index.js and settings for clarity

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 'use strict';
 
 const hof = require('hof');
-const path = require('path');
 
-const settings = require('./hof.settings');
+const addDynamicSettings = (settings) => {
+  settings.routes = settings.routes.map(route => require(route));
+  settings.root = __dirname;
 
-settings.routes = settings.routes.map(route => require(route));
-settings.root = __dirname;
-settings.start = false;
-settings.views = path.resolve(__dirname, './apps/common/views');
-settings.translations = './apps/common/translations';
+  // suppress logs when running the app in silent mode for automation testing, so app logs do not mix with test logs
+  if (process.argv.some(commandLineArgument => commandLineArgument === 'silent')) {
+    settings.loglevel = 'silent';
+  }
 
-// suppress logs when running the app in silent mode for automation testing, so app logs do not mix with test logs
-if (process.argv.some(commandLineArgument => commandLineArgument === 'silent')) {
-  settings.loglevel = 'silent';
-}
+  return settings;
+};
 
-const app = hof(settings);
+const addGenericLocals = (req, res, next) => {
+  res.locals.htmlLang = 'en';
+  res.locals.feedbackUrl = '/feedback';
+  return next();
+};
 
 /*
  During automation test setup, we ping the app with '?automation-test' query to check it is ready before running tests.
  Put a cookie on this request so it doesn't fail HOF-middleware cookie check.
 */
-app.use('/question', (req, res, next) => {
+const addAutomationTestCookie = (req, res, next) => {
   if (req.query['automation-test'] !== undefined) {
     req.cookies.testCookie = 'test';
   }
   return next();
-});
+};
 
-app.use((req, res, next) => {
-  res.locals.htmlLang = 'en';
-  res.locals.feedbackUrl = '/feedback';
-  next();
-});
+const app = hof(addDynamicSettings(require('./hof.settings')));
+
+app.use((req, res, next) => addGenericLocals(req, res, next));
+app.use('/question', (req, res, next) => addAutomationTestCookie(req, res, next));
 
 module.exports = app;
